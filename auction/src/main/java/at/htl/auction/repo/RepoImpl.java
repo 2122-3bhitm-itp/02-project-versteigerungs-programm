867acf402566f732513f116e6a10954fc7377eb4
ValWeinhaeupl <val.weinhaeupl@gmail.com>
2022-05-19 11:06:47 +0200

    Weinhaeupl update

--- auction/src/main/java/at/htl/auction/repo/RepoImpl.java
+++ auction/src/main/java/at/htl/auction/repo/RepoImpl.java
@@ -6,14 +6,14 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class RepoImpl implements Repo{
-    Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/db;create=true", "app", "app");
+    //Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/db;create=true", "app", "app");
 
     public RepoImpl() throws SQLException {
     }
 
     @Override
     public void addauction() throws SQLException {
-        PreparedStatement pstmt = conn.prepareStatement("create table temp(id varchar(12) primary key)");
-        pstmt.executeQuery();
+        //PreparedStatement pstmt = conn.prepareStatement("create table temp(id varchar(12) primary key)");
+        //pstmt.executeQuery();
     }
 }
